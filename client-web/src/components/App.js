55dc5168a18cdf7ffd400350bf4aaf23cfb3bdb1
vvvpiano <vvvpiano60@gmail.com>
2020-05-20 22:30:49 +0900

    refactor: Video.js 삭제

--- client-web/src/components/App.js
+++ client-web/src/components/App.js
@@ -6,7 +6,6 @@ import Login from './pages/Login';
 import Header from './Header';
 import Main from './pages/Main';
 import DirectoryDetail from './pages/DirectoryDetail';
-import Video from './Video';
 
 const App = () => {
     return (
@@ -18,7 +17,6 @@ const App = () => {
                         <Route path="/" exact component={Main} />
                         <Route path="/user_login" exact component={Login} />
                         <Route path="/detail" exact component={DirectoryDetail} />
-                        <Route path="/video_form" exact component={Video} />
                     </Switch>
                 </div>
             </Router>
