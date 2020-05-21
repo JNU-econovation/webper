deb482b18cb3ee02e53bf1c2d8f28e0534d40754
kanamycine <jinyeong5320@gmail.com>
2020-05-21 18:37:51 +0900

    feat:I made navbar.

--- client-web/src/components/App.js
+++ client-web/src/components/App.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import { Router, Route, Switch } from 'react-router-dom';
-import history from '../history';
+import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
+import history from "../history";
 
-import Login from './pages/Login';
-import Header from './Header';
-import Main from './pages/Main';
-import DirectoryDetail from './pages/DirectoryDetail';
+import Login from "./pages/Login";
+import Header from "./Header";
+import Main from "./pages/Main";
+import DirectoryDetail from "./pages/DirectoryDetail";
+import Navbar from "../components/Navbar";
 
 const App = () => {
-    return (
+  return (
+    <div>
+      <Router history={history}>
         <div>
-            <Router history={history}>
-                <div>
-                    <Header />
-                    <Switch>
-                        <Route path="/" exact component={Main} />
-                        <Route path="/user_login" exact component={Login} />
-                        <Route path="/detail" exact component={DirectoryDetail} />
-                    </Switch>
-                </div>
-            </Router>
+          <Header />
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={Main} />
+            <Route path="/user_login" exact component={Login} />
+            <Route path="/detail" exact component={DirectoryDetail} />
+          </Switch>
         </div>
-    );
+      </Router>
+    </div>
+  );
 };
 
 export default App;
