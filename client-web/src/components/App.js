09e56ce0c2dd958777fcc09618838c793361f006
Seoyoung Kim <48192106+vvvpiano@users.noreply.github.com>
2020-05-21 16:23:41 +0900

    Revert "Video kim"

--- client-web/src/components/App.js
+++ client-web/src/components/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import { Router, Route, Switch } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import history from '../history';
 
 import Login from './pages/Login';
-import Header from './Header';
 import Main from './pages/Main';
-import DirectoryDetail from './pages/DirectoryDetail';
 
 const App = () => {
     return (
         <div>
             <Router history={history}>
                 <div>
-                    <Header />
-                    <Switch>
-                        <Route path="/" exact component={Main} />
-                        <Route path="/user_login" exact component={Login} />
-                        <Route path="/detail" exact component={DirectoryDetail} />
-                    </Switch>
+                    <Route path="/user_login" exact component={Login} />
+                    <Route path="/user_main" exact component={Main} />
                 </div>
             </Router>
         </div>
