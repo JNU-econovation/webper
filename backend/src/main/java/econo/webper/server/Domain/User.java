aa041ee985927d212947cb786fa333e621b41507
배종진 <qowhdwlsdk@gmail.com>
2020-05-23 18:24:08 +0900

    feat: Spring Security 적용

--- backend/src/main/java/econo/webper/server/Domain/User.java
+++ backend/src/main/java/econo/webper/server/Domain/User.java
@@ -1,36 +1,36 @@
-package econo.webper.server.Domain;
-
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
-
-@Getter @Builder @NoArgsConstructor
-@EqualsAndHashCode(of = "id")
-@Entity
-public class User {
-
-    @Id
-    @GeneratedValue
-    private Integer id;
-
-    @NotNull
-    private String userId;
-
-    @NotNull
-    private String password;
-
-    @NotNull
-    private String name;
-
-    @NotNull
-    private String email;
-
-    private String introduction;
-}
-
+//package econo.webper.server.Domain;
+//
+//import lombok.Builder;
+//import lombok.EqualsAndHashCode;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.Id;
+//import javax.validation.constraints.NotNull;
+//
+//@Getter @Builder @NoArgsConstructor
+//@EqualsAndHashCode(of = "id")
+//@Entity
+//public class User {
+//
+//    @Id
+//    @GeneratedValue
+//    private Integer id;
+//
+//    @NotNull
+//    private String userId;
+//
+//    @NotNull
+//    private String password;
+//
+//    @NotNull
+//    private String name;
+//
+//    @NotNull
+//    private String email;
+//
+//    private String introduction;
+//}
+//
