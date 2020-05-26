fc0e40256ea9f7ecd8f154b7e875075690974661
배종진 <qowhdwlsdk@gmail.com>
2020-05-26 17:17:07 +0900

    refactor: SecurityConfig 불필요한 import 구문 삭제

--- backend/src/main/java/econo/webper/server/config/SecurityConfig.java
+++ backend/src/main/java/econo/webper/server/config/SecurityConfig.java
@@ -1,4 +1,19 @@
 package econo.webper.server.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers( "/api/login").anonymous()
+                .anyRequest().authenticated();
+    }
 }
