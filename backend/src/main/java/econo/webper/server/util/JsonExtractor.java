a1200d5da3a8f94760bfaf6dc7cd0be1cb56b997
배종진 <qowhdwlsdk@gmail.com>
2020-05-27 11:50:27 +0900

    Refactor: JsonExtractor 클래스 구현 및 컨트롤러 리팩토링

--- backend/src/main/java/econo/webper/server/util/JsonExtractor.java
+++ backend/src/main/java/econo/webper/server/util/JsonExtractor.java
@@ -1,4 +1,21 @@
 package econo.webper.server.util;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.springframework.stereotype.Component;
+
+@Component
 public class JsonExtractor {
+
+    private static JSONParser jsonParser = new JSONParser();
+
+    public static Object getValueByKey(String body, String key) {
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = (JSONObject) jsonParser.parse(body);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return jsonObject.get(key);
+    }
 }
