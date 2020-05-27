e0e5e15e3e2176690c4fd2fbecb15b693dbd0294
Seoyoung Kim <48192106+vvvpiano@users.noreply.github.com>
2020-05-27 16:28:26 +0900

    style: Navbar style 수정

--- client-web/src/components/Navbar.js
+++ client-web/src/components/Navbar.js
@@ -8,56 +8,26 @@ const Navbar = () => {
           <h2>Directory</h2>
           <div>
             <a href="#">Videos</a>
-            <button>...</button>
-            <button>+</button>
+            <button class="rightend">...</button>
+            <button class="rightend">+</button>
           </div>
           <ul>
             <li>
               <a href="#">test1</a>
-              <button>...</button>
-              <button>+</button>
+              <button class="rightend">...</button>
+              <button class="rightend">+</button>
             </li>
             <li>
               <a herf="#">test2</a>
-              <button>...</button>
-              <button>+</button>
-            </li>
-            <li>
-              <a herf="#">test3</a>
-              <button>...</button>
-              <button>+</button>
-            </li>
-            <li>
-              <a herf="#">test4</a>
-              <button>...</button>
-              <button>+</button>
+              <button class="rightend">...</button>
+              <button class="rightend">+</button>
             </li>
           </ul>
 
           <div>
             <a href="#" />
-            Blogs<button>...</button>
-            <button>+</button>
-          </div>
-          <div>
-            <a href="#" />
-            Wishlist<button>...</button>
-            <button>+</button>
-          </div>
-          <div>
-            <a href="#" />
-            Portals<button>...</button>
-            <button>+</button>
-          </div>
-          <div>
-            <a href="#" />
-            News<button>...</button>
-            <button>+</button>
-          </div>
-          <div>
-            <a href="#" />
-            JobHuntingInfo<button>...</button>
-            <button>+</button>
+            Blogs<button class="rightend">...</button>
+            <button class="rightend">+</button>
           </div>
         </ul>
       </nav>
