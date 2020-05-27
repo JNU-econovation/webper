c64a6b2c0bf594a577273694db08c0690c0f0b93
배종진 <qowhdwlsdk@gmail.com>
2020-05-27 21:00:17 +0900

    feat: GoogleUserinfoDTO 클래스 생성

--- backend/src/main/java/econo/webper/server/login/GoogleUserinfoDTO.java
+++ backend/src/main/java/econo/webper/server/login/GoogleUserinfoDTO.java
@@ -1,4 +1,13 @@
 package econo.webper.server.login;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class GoogleUserinfoDTO {
+
+    private String name;
+
+    private String email;
 }
