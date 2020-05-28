8cd7e0005c9d9ccd3fd42475dd9667f6597f0c42
배종진 <qowhdwlsdk@gmail.com>
2020-05-28 09:21:04 +0900

    test: GoogleLoginService의 올바르지 못한 인증 요청시 Error Status 테스트

--- backend/src/test/java/econo/webper/server/login/GoogleLoginServiceTest.java
+++ backend/src/test/java/econo/webper/server/login/GoogleLoginServiceTest.java
@@ -1,5 +1,25 @@
-import static org.junit.Assert.*;
+package econo.webper.server.login;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class GoogleLoginServiceTest {
+
+    @Autowired
+    GoogleLoginService googleLoginService;
+
+    @Test
+    public void authenticateWithWrongAccessToken() {
+        ResponseEntity responseEntity = googleLoginService.authenticate("sdjklfdsjklfsdjkl");
+        assertThat(responseEntity.getStatusCode().is4xxClientError()).isTrue();
+    }
 
 }
