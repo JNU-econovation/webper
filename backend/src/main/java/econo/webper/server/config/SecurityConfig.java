cbff8355cb9056ebe525a6af57937d2e82040743
배종진 <qowhdwlsdk@gmail.com>
2020-05-28 17:42:27 +0900

    refactor: Spring Security csrf Protection disable

--- backend/src/main/java/econo/webper/server/config/SecurityConfig.java
+++ backend/src/main/java/econo/webper/server/config/SecurityConfig.java
@@ -14,6 +14,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
             .authorizeRequests()
                 .antMatchers( "/login/google").anonymous()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+            .and().csrf().disable();
     }
 }
