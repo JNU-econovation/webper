cea7568b1500b000544bb3b033062ed5e6e765c6
배종진 <qowhdwlsdk@gmail.com>
2020-06-29 16:45:48 +0900

    Refactor: UserDetails를 구현하는 User 리팩토링

--- backend/src/main/java/econo/webper/server/domain/User.java
+++ backend/src/main/java/econo/webper/server/domain/User.java
@@ -1,14 +1,21 @@
 package econo.webper.server.domain;
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter @Builder
 @NoArgsConstructor @AllArgsConstructor @EqualsAndHashCode(of = "id")
 @Entity
-public class User {
+public class User implements UserDetails{
 
     @Id
     @GeneratedValue
@@ -25,5 +32,38 @@ public class User {
 
     private String introduction;
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
 
