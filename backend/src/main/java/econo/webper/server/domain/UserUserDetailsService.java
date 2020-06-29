a010eaf511f6466629b5f34c07bf86bc1af5ed5b
배종진 <qowhdwlsdk@gmail.com>
2020-06-29 16:46:33 +0900

    feat: UserUserDetailsService 구현

---
+++ backend/src/main/java/econo/webper/server/domain/UserUserDetailsService.java
@@ -0,0 +1,21 @@
+package econo.webper.server.domain;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    public UserUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userService.findUserByEmail(email);
+    }
+}
