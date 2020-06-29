904a2970a2a2003b22260900205fa1c0bd0adff9
배종진 <qowhdwlsdk@gmail.com>
2020-06-29 16:44:31 +0900

    feat: Security Custom Filter 등록

--- backend/src/main/java/econo/webper/server/config/SecurityConfig.java
+++ backend/src/main/java/econo/webper/server/config/SecurityConfig.java
@@ -1,20 +1,40 @@
 package econo.webper.server.config;
 
+import econo.webper.server.jwt.JwtAuthenticationFilter;
+import econo.webper.server.jwt.JwtTokenProvider;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests()
-                .antMatchers( "/login/google").anonymous()
-                .anyRequest().authenticated()
-            .and().csrf().disable();
+                .httpBasic().disable()
+                .csrf().disable();
+
+        http
+                .authorizeRequests()
+                    .antMatchers( "/login/google").anonymous()
+                    .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
     }
 }
