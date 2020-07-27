b1a10c6ed31f0bce0ef13f2b56d153293deefb5e
JongJin Bae <qowhdwlsdk@gmail.com>
2020-07-27 16:48:49 +0900

    config:CORS 설정

--- backend/src/main/java/econo/webper/server/config/AppConfig.java
+++ backend/src/main/java/econo/webper/server/config/AppConfig.java
@@ -3,6 +3,8 @@ package econo.webper.server.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +12,20 @@ public class AppConfig {
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowCredentials(false)
+                        .maxAge(3000);
+            }
+        };
+
     }
 }
