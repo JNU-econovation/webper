5d753b18465a2b134e5d7e96f8a9d90ea83e9ab6
Seoyoung Kim <48192106+vvvpiano@users.noreply.github.com>
2020-07-27 19:17:38 +0900

    cookies set and remove in loginbutton

--- client-web/src/components/pages/Login.js
+++ client-web/src/components/pages/Login.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import LoginButton from '../Header/LoginButton';
+import { connect } from 'react-redux';
 
 class Login extends React.Component {
     render() {
         return (
             <div className="center">
                 <div className="webperLogo centered"><img src="images/logo.png" alt="logo" /></div>
-                <LoginButton />
+                <LoginButton cookies={this.props.cookies} />
             </div>
         )
     }
 }
 
-export default Login;
+const mapStateToProps = (state, ownProps) => {
+    console.log("cookies:", ownProps.cookies);
+    return {
+        cookies: ownProps.cookies
+    };
+}
+
+export default connect(mapStateToProps, null)(Login);
