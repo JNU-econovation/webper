b57a20952a5a0afbc6ec718d410d7e59f7629427
Seoyoung Kim <48192106+vvvpiano@users.noreply.github.com>
2020-07-29 18:03:56 +0900

    feat: url Drag-and-Drop으로 스크랩 생성 기능 추가

--- client-web/src/components/UrlForm.js
+++ client-web/src/components/UrlForm.js
@@ -1,8 +1,16 @@
 import React from "react";
 
 import { Field, reduxForm } from "redux-form";
+import UrlDnd from "./UrlDnd";
 
 class UrlForm extends React.Component {
+
+  state = { onDnd: false };
+
+  setDndMod = () => {
+    return (this.state.onDnd ? this.setState({ onDnd: false }) : this.setState({ onDnd: true }));
+  }
+
   renderURLInput = ({ input }) => {
     return (
       <div>
@@ -22,15 +30,27 @@ class UrlForm extends React.Component {
     this.props.initialize();
   };
 
+
+  renderContents = () => {
+    if (this.state.onDnd === false) {
+      return (
+        <UrlDnd onSubmit={this.onSubmit} />
+      )
+    }
+    return (
+      <div className="urlform">
+        <form onSubmit={this.props.handleSubmit(this.onSubmit)} onClick={(e) => e.stopPropagation()}>
+          <Field name="inputURL" component={this.renderURLInput} />
+          <button onSubmit={this.onSubmit}>Create</button>
+        </form>
+      </div>
+    )
+  }
+
   render() {
     return (
-      <div className="scrap-url">
-        <div className="urlform">
-          <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
-            <Field name="inputURL" component={this.renderURLInput} />
-            <button onSubmit={this.onSubmit}>Create</button>
-          </form>
-        </div>
+      <div className="scrap-url" onClick={this.setDndMod}>
+        {this.renderContents()}
       </div>
     );
   }
