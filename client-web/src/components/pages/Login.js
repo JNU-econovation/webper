54d893be9e6f4d575871890692770d6134ea345a
kanamycine <jinyeong5320@gmail.com>
2020-07-30 23:05:11 +0900

    css : logo align, login button modify, login icon size adjust etc

--- client-web/src/components/pages/Login.js
+++ client-web/src/components/pages/Login.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import LoginButton from '../Header/LoginButton';
+import React from "react";
+import LoginButton from "../Header/LoginButton";
 
 class Login extends React.Component {
-    render() {
-        return (
-            <div className="center">
-                <div className="webperLogo centered"><img src="images/logo.png" alt="logo" /></div>
-                <LoginButton />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="center">
+        <div className="webperLogo centered">
+          <img className="webperLogo" src="images/logo.png" alt="logo" />
+        </div>
+        <div>
+          <button className="google-login-button">
+            <LoginButton className="google-login-content" />
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Login;
