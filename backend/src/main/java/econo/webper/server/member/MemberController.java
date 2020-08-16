85a9d60b40cc18cbf082d692110d21180e00bd0a
JongJin Bae <qowhdwlsdk@gmail.com>
2020-08-16 17:06:22 +0900

    refactor: Update Response Process in MemberController

--- backend/src/main/java/econo/webper/server/member/MemberController.java
+++ backend/src/main/java/econo/webper/server/member/MemberController.java
@@ -1,10 +1,14 @@
-package econo.webper.server.Member;
+package econo.webper.server.member;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import econo.webper.server.directory.DirectoryService;
 import econo.webper.server.directory.dto.DirectoryDTO;
-import econo.webper.server.utils.ExceptionMessage;
+import econo.webper.server.security.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,19 +24,24 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    public MemberController(DirectoryService directoryService, MemberService memberService) {
+    private final ObjectMapper objectMapper;
+
+    public MemberController(DirectoryService directoryService, MemberService memberService, ObjectMapper objectMapper) {
         this.directoryService = directoryService;
         this.memberService = memberService;
+        this.objectMapper = objectMapper;
     }
 
     @GetMapping("/root-directory")
     @ApiImplicitParam(name = "Authorization", value = "Access_Token", required = true, paramType = "header")
-    public ResponseEntity getRootDirectories(@AuthenticationPrincipal MemberDetails memberDetails) {
-        Member savedMember = memberService.findMemberByEmail(memberDetails.getMember().getEmail());
-        List<DirectoryDTO> directoryDTOs = directoryService.getDirectoryDTOs(savedMember);
-        if (directoryDTOs.size() == 0) {
-            return ResponseEntity.badRequest().body(ExceptionMessage.NOT_GET_ROOT_DIRECTORY);
+    public ResponseEntity getRootDirectories(@AuthenticationPrincipal User user) throws JsonProcessingException {
+        Member member = memberService.findMemberByEmail(user.getUsername());
+        List<DirectoryDTO> directoryDtoList = directoryService.getDirectoryDTOs(member);
+        if (directoryDtoList.size() == 0) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         }
-        return ResponseEntity.ok(directoryDTOs);
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(objectMapper.writeValueAsString(directoryDtoList));
     }
 }
