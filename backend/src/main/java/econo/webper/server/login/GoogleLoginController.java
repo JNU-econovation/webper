b69bae8fc5dfb45eeaf99c64f0db71627c3cd211
JongJin Bae <qowhdwlsdk@gmail.com>
2020-08-20 16:01:56 +0900

    refactor: Update HttpStatus Code and Add MediaType in GoogleLoginController

--- backend/src/main/java/econo/webper/server/login/GoogleLoginController.java
+++ backend/src/main/java/econo/webper/server/login/GoogleLoginController.java
@@ -8,6 +8,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,7 +35,7 @@ public class GoogleLoginController {
     @ApiOperation(value = "Login By Google OAuth", response = ResponseEntity.class)
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Login Success"),
-            @ApiResponse(code = 404, message = "Login Failed")
+            @ApiResponse(code = 401, message = "Login Failed")
     })
     @PostMapping("/login/google")
     public ResponseEntity loginByGoogleOAuth(@RequestBody GoogleAccessTokenDTO googleAccessTokenDTO) {
@@ -43,9 +45,9 @@ public class GoogleLoginController {
             List<MemberRole> memberRoles = Collections.singletonList(MemberRole.USER);
             memberService.saveMember(googleUserinfoDTO, memberRoles);
             String token = jwtTokenProvider.createToken(googleUserinfoDTO.getEmail(), memberRoles);
-            return ResponseEntity.ok(convertStringToMap(token));
+            return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).body(convertStringToMap(token));
         }
-        return ResponseEntity.badRequest().body(ExceptionMessage.GOOGLE_OAUTH_ACCESS_TOKEN_UNVALIDATED);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ExceptionMessage.GOOGLE_OAUTH_ACCESS_TOKEN_UNVALIDATED);
     }
 
     private Map<String, String> convertStringToMap(String token) {
--- backend/src/main/java/econo/webper/server/login/GoogleUserinfoDTO.java
+++ backend/src/main/java/econo/webper/server/login/GoogleUserinfoDTO.java
@@ -5,7 +5,15 @@ import lombok.Setter;
 
 @Getter
 @Setter
+
 public class GoogleUserinfoDTO {
 
     private String email;
+
+    public GoogleUserinfoDTO() {
+    }
+
+    public GoogleUserinfoDTO(String email) {
+        this.email = email;
+    }
 }
