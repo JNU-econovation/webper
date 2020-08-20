6ab1e4268be5f4d3c757b906f6eb97f6279ef985
JongJin Bae <qowhdwlsdk@gmail.com>
2020-08-20 22:16:20 +0900

    refactor: restTemplate in GoogleLoginService

--- backend/src/main/java/econo/webper/server/login/GoogleLoginService.java
+++ backend/src/main/java/econo/webper/server/login/GoogleLoginService.java
@@ -2,12 +2,12 @@ package econo.webper.server.login;
 
 import econo.webper.server.login.dto.GoogleUserinfoDTO;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -18,8 +18,8 @@ public class GoogleLoginService {
     @Value("${social.google.url}")
     private String googleRequestUrl;
 
-    public GoogleLoginService() {
-        restTemplate = new RestTemplate();
+    public GoogleLoginService(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
     }
 
     public ResponseEntity authenticate(String accessToken) {
@@ -27,8 +27,8 @@ public class GoogleLoginService {
         httpHeaders.set("Authorization", "Bearer " + accessToken);
         HttpEntity request = new HttpEntity(httpHeaders);
         try {
-            return restTemplate.exchange(googleRequestUrl, HttpMethod.GET, request, GoogleUserinfoDTO.class, 1);
-        } catch (HttpStatusCodeException e) {
+            return restTemplate.exchange(googleRequestUrl, HttpMethod.GET, request, GoogleUserinfoDTO.class);
+        } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
     }
